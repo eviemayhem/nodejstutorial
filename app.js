d4d073c855202bbe10b662606c558ce761750fed
Evie <iles.hollyl@gmail.com>
2021-12-06 21:24:29 -0500

    Added mongodb and mongoose

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-const { response } = require('express');
-const express = require('express');
-const app = express();
+import express, { static } from 'express';
 
+import morgan from 'morgan';
+import { connect } from 'mongoose';
+import { save } from './models/blog';
+
+
+const app = express();
+//connect to mongdb
+const dbuRI ="mongodb+srv://evie:<evieevie>@cluster0.fdas3.mongodb.net/node-tuts?retryWrites=true&w=majority";
+connect(dbuRI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(db => app.listen(3000, () => {
+    console.log('Server is up on port 3000, db connected');
+}))
+.catch(err => console.log(err));
 //register view engine
 app.set('view engine', 'ejs');
+
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+ const blog = new Blog({
+     title: 'Test Blog',
+     snippet: 'Test snippet',
+     body: 'Test body'
+ })
+});
+
+save().then(result => {
+    console.log(result);
+})
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//search for blog
+app.get('/search', (req, res) => {
+    const { search } = req.query;
+    Blog.find({title: {$regex: search, $options: 'i'}})
+    .then(blogs => {
+        res.render('index', {blogs});
+    })
+    
+
+
+
+//middleware and static files
+
+app.use(static('public'));
+app.use(morgan('dev'));
+
 
 
 
